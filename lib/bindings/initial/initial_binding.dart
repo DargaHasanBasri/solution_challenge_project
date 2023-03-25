d54c0fde765543c9d4d160c066a30423bfd20c69
Hasan Basri <85760132+DargaHasanBasri@users.noreply.github.com>
2023-03-25 17:10:32 +0300

    first commit

---
+++ lib/bindings/initial/initial_binding.dart
@@ -0,0 +1,8 @@
+import 'package:solution_challenge_project/export.dart';
+
+class InitialBinding implements Bindings {
+  @override
+  void dependencies() {
+    //Get.put(ConnectionController());
+  }
+}
---
+++ lib/core/init/ui_helper/ui_helper.dart
@@ -0,0 +1,17 @@
+import 'package:solution_challenge_project/export.dart';
+
+class UiHelper {
+  static const Size designSize = Size(375, 812);
+
+  static TextStyle headline1 = TextStyle(
+    fontSize: 24.sp,
+  );
+
+  static SizedBox defaultSpaceHeight = SizedBox(
+    height: 12.h,
+  );
+
+  static SizedBox defaultSpaceWidth = SizedBox(
+    height: 12.w,
+  );
+}
